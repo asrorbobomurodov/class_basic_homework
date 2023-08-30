c8b2522c76c84b4be3da40ddc5f0eeb7e688ecf2
Asror Bobomurodov <asrorbobomurodov>
2023-08-30 07:12:00 +0500

    done by @asror

--- create_two_attribute.py
+++ create_two_attribute.py
@@ -1,3 +1,7 @@
 #Create a "Person" class
-
+class Person():
 #that has a name("name") and a age("age")
+    def __init__(self, name, age):
+        self.name = name
+        self.name = age
+        
